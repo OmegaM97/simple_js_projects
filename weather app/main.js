4d251d41668907d70110911569fcd1f95054b8c9
OmegaM97 <omegamelese68@gmail.com>
2025-05-06 16:36:17 +0300

    make new type of weather, catch error for the fetch and loading screen

--- weather app/main.js
+++ weather app/main.js
@@ -1,21 +1,55 @@
 let weatherInfo = {};
 
+document.querySelector('.js-search-input')
+.addEventListener('keydown', (event) => {
+  if(event.key == 'Enter') {
+    searchTheWeather();
+  } 
+})
+
 document.querySelector('.js-search-button')
 .addEventListener('click', () => {
+  searchTheWeather();
+})
+
+
+function searchTheWeather() {
+  document.querySelector('.generate-div')
+  .innerHTML = `
+  <p class="loading-para">Loading...</p>
+      <div class="loading-container">
+        <div class="loading-circle">
+        </div>
+      </div>
+  `;
+
   const inputBtn = document.querySelector('.js-search-input');
   let city = inputBtn.value.split('-');
-  console.log(city);
 
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city[0]}&appid=4775bdf04db21ebed84adef3712debfd&units=metric`).then((response) => {
+    if (!response.ok) {
+      throw response;
+    }
     return response.json();
   }).then((data) => {
     weatherInfo = data;
     renderOnPage();
-
     inputBtn.value = '';
+  }).catch((error) => {
+
+    if(error.status >= 400 && error.status < 500) {
+      document.querySelector('.generate-div')
+      .innerHTML = `<p class='error-message'>Please select a valid city from the list!!</p>`;
+      inputBtn.value = '';
+    } else {
+      document.querySelector('.generate-div')
+      .innerHTML = `<p class='error-message'>Sorry, try again later</p>`;
+      inputBtn.value = '';
+    }
+    
   });
 
-})
+}
 
 function renderOnPage() {
   let description = simplifyWeather(weatherInfo.weather[0].description);
@@ -34,12 +68,12 @@ function renderOnPage() {
     <div class="weather-description inner-container">${description}</div>
 
     <br>
-    <div class="temprature inner-container">Feels like ${weatherInfo.main.feels_like}<sup>&deg;</sup>C</div>
+    <div class="temprature inner-container">Feels like ${roundNum(weatherInfo.main.feels_like)}<sup>&deg;</sup>C</div>
 
     <div class="humidity inner-container">Humidity: ${weatherInfo.main.humidity}%</div>
 
     <div class="wind-speed inner-container">
-      Wind speed: ${weatherInfo.wind.speed}km/h
+      Wind speed: ${weatherInfo.wind.speed}m/s
     </div>
   </div>
   `
@@ -51,17 +85,25 @@ function renderOnPage() {
 function simplifyWeather(description) {
   description = description.toLowerCase();
 
-  const cloudy = ["few clouds", "scattered clouds", "overcast clouds"];
+  const cloudy = ["broken clouds", "overcast clouds"];
+  const clouds = ["few clouds", "scattered clouds"]
   const rain = ["light rain", "moderate rain", "heavy rain"];
   const thunderstorm = ["thunderstorm", "thunderstorm with rain"];
   const snow = ["light snow", "snow", "heavy snow"];
+  const drizzle = ["light intensity drizzle", "drizzle","heavy intensity drizzle", "light intensity drizzle rain", "drizzle rain", "heavy intensity drizzle rain"]
 
-  if (description === "clear sky") return "Clear";
+  if (description === "clear sky") return "Sunny";
   if (cloudy.includes(description)) return "Cloudy";
+  if (clouds.includes(description)) return "Partly Cloudy";
   if (rain.includes(description)) return "Rain";
+  if (drizzle.includes(description)) return "Drizzle";
   if (thunderstorm.includes(description)) return "Thunderstorm";
   if (snow.includes(description)) return "Snow";
 
   // Default fallback
   return "Windy";
+}
+
+function roundNum(number) {
+  return Math.round(number);
 }
